d4d679e42524ea12df945eae932beb42ec3d9c99
Michael Edes <michael.edes@boxmodel.com>
2023-08-17 00:41:17 +0100

    chore: layout and nav component

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,1 +1,56 @@
-export const NavBar = () => {};
+import React, { useState, useEffect } from "react";
+import styles from "./NavBar.module.scss";
+
+interface CustomLinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const NavBar = () => {
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [showNav, setShowNav] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollTop = window.pageYOffset;
+
+      if (currentScrollTop > lastScrollTop) {
+        setShowNav(false);
+      } else {
+        setShowNav(true);
+      }
+
+      setLastScrollTop(currentScrollTop);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollTop]);
+
+  return (
+    <header className={`${styles.header} ${showNav ? "" : styles.hide}`}>
+      <div className={styles.navContainer}>
+        <a href="/">Home</a>
+        <nav>
+          <ul>
+            <CustomLink href="#intro">Intro</CustomLink>
+            <CustomLink href="#projects">Projects</CustomLink>
+            <CustomLink href="#contact">Contact</CustomLink>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+function CustomLink({ href, children, ...props }: CustomLinkProps) {
+  return (
+    <li>
+      <a href={href}>{children}</a>
+    </li>
+  );
+}
+export default NavBar;
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import styled from "./Home.module.scss";
 import "../../styles/global.scss";
+import NavBar from "../../components/NavBar/NavBar";
 
 function Home() {
   return (
-    <div className="App">
-      <header className={styled.header}>
-        <div>
-          <h1 className="hello">hello</h1>
-        </div>
-      </header>
+    <div className={styled.container}>
+      <NavBar />
+      <main className={styled.content}>
+        <section className={`${styled.intro}`} id="intro">
+          <h1>Intro</h1>
+        </section>
+        <section className={`${styled.projects}`} id="projects">
+          <h1>Projects</h1>
+        </section>
+        <section className={`${styled.contact}`} id="contact">
+          <h1>Contact</h1>
+        </section>
+      </main>
     </div>
   );
 }
