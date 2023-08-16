67e8a7751a4909b382e6d90360082f3e300ab2da
Michael Edes <michael.edes@boxmodel.com>
2023-08-16 21:17:15 +0100

    chore: remove boilerplate code, setup sass

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.module.scss";
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
