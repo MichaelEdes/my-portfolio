5a1579ef6129354590f645526561264f87fbac84
Michael Edes <michael.edes@boxmodel.com>
2023-08-21 14:56:59 +0100

    chore: updating projects

--- client/src/data/projects.tsx
+++ client/src/data/projects.tsx
@@ -2,6 +2,23 @@ import { Technologies } from "../types/technologies";
 
 export const projects = [
   {
+    cardTitle: "Portfolio",
+    cardText:
+      "My most recent project, my portfolio displaying current and previous projects",
+    projectLink: "https://michaeledes.com",
+    projectImage: "https://iili.io/Hph2Z41.png",
+    technologies: [
+      Technologies.React,
+      Technologies.TypeScript,
+      Technologies.MySQL,
+      Technologies.Node,
+      Technologies.JawsDB,
+    ],
+    description:
+      "Welcome to my personal portfolio, a reflection of my journey in the tech world. Here, you'll discover a compilation of projects I've undertaken, including this very portfolio. Crafted with React and TypeScript for a seamless interface, it's powered by MySQL with backend processes streamlined via Node.js and further enhanced with JawsDB. Dive in to understand my expertise and evolution as a developer.",
+    repoUrl: "https://github.com/MichaelEdes/my-portfolio",
+  },
+  {
     cardTitle: "SMRC",
     cardText: "E-commerce website for device repair store",
     projectLink: "https://sunderlandmobiles.com",
@@ -17,6 +34,7 @@ export const projects = [
       "The SMRC platform offers an e-commerce solution for a specialized device repair store. Through this user-friendly site, customers can seamlessly access repair services for their devices. Built with cutting-edge technologies like React and Node, the website ensures smooth navigation and a secure database with MySQL and JawsDB, enhancing the overall user experience. Explore its features at sunderlandmobiles.com.",
     repoUrl: "https://github.com/MichaelEdes/smrc-site",
   },
+
   {
     cardTitle: "Travel Blog",
     cardText:
@@ -35,40 +53,6 @@ export const projects = [
     repoUrl: "https://github.com/MichaelEdes/my-blog",
   },
   {
-    cardTitle: "Wave",
-    cardText: "A psychological health and wellness tracker",
-    projectLink: "https://github.com/MichaelEdes/Wave",
-    projectImage: "https://iili.io/HbJBUAP.jpg",
-    technologies: [Technologies.Swift, Technologies.AdobeXD],
-    description:
-      "Wave is an innovative psychological wellness application. Built with Swift and designed using AdobeXD, the application aims to empower users to track and improve their mental health. With a combination of intuitive design and scientific metrics, Wave offers users comprehensive insights into their psychological well-being. Source code available on GitHub.",
-    repoUrl: "https://github.com/MichaelEdes/Wave",
-  },
-  {
-    cardTitle: "SalesLynk",
-    cardText:
-      "SaaS solution developed to help cyber security professionals build stronger relationships with their clients and prospects",
-    projectLink: "https://www.saleslynk.co.uk/",
-    projectImage: "https://iili.io/HDm9FTP.jpg",
-    technologies: [
-      Technologies.React,
-      Technologies.TypeScript,
-      Technologies.Node,
-    ],
-    description:
-      "SalesLynk is a bespoke SaaS platform catering to cybersecurity professionals. It's designed to foster and strengthen client relationships, with an emphasis on trust and communication. Utilizing technologies like React, TypeScript, and Node, SalesLynk provides a seamless interface for professionals to offer and receive services. Dive into its offerings at saleslynk.co.uk.",
-  },
-  {
-    cardTitle: "Infocrypt",
-    cardText: "A mobile NFT marketplace",
-    projectLink:
-      "https://www.figma.com/proto/Gf0WZiGpJDTcZHQDgEEneH/Untitled?page-id=0%3A1&node-id=2-106&viewport=241%2C48%2C0.41&scaling=scale-down&starting-point-node-id=2%3A106",
-    projectImage: "https://iili.io/HDb8nRV.png",
-    technologies: [Technologies.Figma],
-    description:
-      "Stepping into the rapidly evolving world of NFTs, Infocrypt offers a mobile marketplace for digital collectors and creators. With a design prototype available on Figma, Infocrypt showcases an easy-to-use platform for trading and exploring NFTs.",
-  },
-  {
     cardTitle: "Metro",
     cardText: "Online website for north east sandwich shop",
     projectLink: "http://metrosandwichco.co.uk",
@@ -85,19 +69,24 @@ export const projects = [
     repoUrl: "https://github.com/MichaelEdes/metro",
   },
   {
-    cardTitle: "Indrix - WIP",
-    cardText: "Futuristic concept e-commerce store",
-    projectLink: "https://www.indrex.co.uk",
-    projectImage: "https://iili.io/HDp5IYx.png",
-    technologies: [
-      Technologies.React,
-      Technologies.TypeScript,
-      Technologies.MySQL,
-      Technologies.Node,
-      Technologies.JawsDB,
-    ],
+    cardTitle: "Wave",
+    cardText: "A psychological health and wellness tracker",
+    projectLink: "https://github.com/MichaelEdes/Wave",
+    projectImage: "https://iili.io/HbJBUAP.jpg",
+    technologies: [Technologies.Swift, Technologies.AdobeXD],
     description:
-      "Indrex promises a futuristic e-commerce experience. Still a work in progress, the store aims to incorporate advanced tech features and a user-friendly interface. Designed with technologies like React, TypeScript, and MySQL, the platform intends to redefine the standard for online shopping. Sneak a peek at indrex.co.uk.",
-    repoUrl: "https://github.com/MichaelEdes/indrix",
+      "Wave is an innovative psychological wellness application. Built with Swift and designed using AdobeXD, the application aims to empower users to track and improve their mental health. With a combination of intuitive design and scientific metrics, Wave offers users comprehensive insights into their psychological well-being. Source code available on GitHub.",
+    repoUrl: "https://github.com/MichaelEdes/Wave",
+  },
+
+  {
+    cardTitle: "Infocrypt",
+    cardText: "A mobile NFT marketplace",
+    projectLink:
+      "https://www.figma.com/proto/Gf0WZiGpJDTcZHQDgEEneH/Untitled?page-id=0%3A1&node-id=2-106&viewport=241%2C48%2C0.41&scaling=scale-down&starting-point-node-id=2%3A106",
+    projectImage: "https://iili.io/HDb8nRV.png",
+    technologies: [Technologies.Figma],
+    description:
+      "Stepping into the rapidly evolving world of NFTs, Infocrypt offers a mobile marketplace for digital collectors and creators. With a design prototype available on Figma, Infocrypt showcases an easy-to-use platform for trading and exploring NFTs.",
   },
 ];
